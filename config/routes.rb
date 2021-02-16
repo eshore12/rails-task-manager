4a1aa5379bd851416002884644e7b2e8eafb86a8
Evan Shore <evandaishore12@gmail.com>
2021-02-16 15:09:44 +0900

    got logic to work- no design whatsoever

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Get all
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # Create new task
+  # form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+
+  # creation
+  post '/tasks', to: 'tasks#create'
+
+  #Get one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  #delete/destory
+  delete '/tasks/:id', to: 'tasks#destroy'
+
 end
